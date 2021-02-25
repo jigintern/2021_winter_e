f91d9032887f87a3f6fc2958007edd68eb5cddad
asann3 <39186984+asann3@users.noreply.github.com>
2021-02-26 00:29:35 +0900

    Co-authored-by: nimuyohu <nimuyohu@users.noreply.github.com>

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,50 @@
 import { Server } from "https://js.sabae.cc/Server.js";
+
+/**
+ * API
+ *	name getEmotionsValue
+ *　req: 文章
+ *  res: value 感情を表す数値(2つ) api Google Cloud Nature Language
+ * 		 値の意味
+ *      -1.0~1.0
+ *  score:
+ *  magnitude:
+ * 	   https://cloud.google.com/natural-language?hl=ja
+ *
+ * Deno.run 使えばコマンド呼出しも可能
+ */
+
+// - APIの確認
+
+// - エラー処理
+// - クライアントとサーバーとのやりとり(JSON)
+
+// json整形関数をつくる
+
 class MyServer extends Server {
-	api(path) {
-		return { name: "jigintern", path: path };
+	api(path, req) {
+		console.log("path", path);
+		if (path == "/api/getEmotionsValue") {
+			//　定義
+			//　送られてきた文章
+			// let sentense = req.sentense;
+			// let emotinalValue;
+
+			// 　（数値を取得）
+			// use API
+
+			// 数値を返す
+			// return { value: emotinalValue };
+
+			// 仮のresponse
+			// return { value: 1.0 };
+			console.log("val", process.env.API_KEY);
+			console.log(req);
+			const res = getEmotionsValue("asdfasdf", process.env.API_KEY);
+			console.log("hoge");
+			return res;
+		}
+		return { err: "not found" };
 	}
 }
 
