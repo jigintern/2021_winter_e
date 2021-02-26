d3e18017fbafc34af51d9a870133a6aa9ca0718f
asann3 <39186984+asann3@users.noreply.github.com>
2021-02-26 17:51:49 +0900

    コメント削除, letをconstに変更

--- Server/server.js
+++ Server/server.js
@@ -21,61 +21,17 @@ const getEmotionsValue = async (sentenses, access_token) => {
 	}
 };
 
-/**
- * API
- *	name getEmotionsValue
- *　req: 文章
- *  res: value 感情を表す数値(2つ) api Google Cloud Nature Language
- * 		 値の意味
- *      -1.0~1.0
- *  score:
- *  magnitude:
- * 	   https://cloud.google.com/natural-language?hl=ja
- *
- * Deno.run 使えばコマンド呼出しも可能
- */
-
-// - APIの確認
-
-// - エラー処理
-// - クライアントとサーバーとのやりとり(JSON)
-
-// json整形関数をつくる
-
 class MyServer extends Server {
 	async api(path, req) {
 		if (path == "/api/getEmotionsValue") {
 			try {
-				//　定義
-				//　送られてきた文章
-				let sentense = req.sentense;
-				// let emotinalValue;
-
-				// 　（数値を取得）
-				// use API
-
-				// 数値を返す
-				// return { value: emotinalValue };
-
-				// 仮のresponse
-				// return { value: 1.0 };
-				
+				const sentense = req.sentense;
 				const result = await getEmotionsValue(sentense, Deno.env.get("API_KEY"));
-
-				// result.then((data) => {
-				// 	console.log(data.documentSentiment.score)
-				// 	return {score:data.documentSentiment.score};
-				// }).catch((err) => {
-				// 	return console.log(err);
-				// });
 				return result
-
 			} catch (e) {
 				console.log(e)
 				return err.message
 			}
-			
-			
 		}
 		return { err: "not found" };
 	}
